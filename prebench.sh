68af0b8554a8a8baf9dea54fa7a4ff4ca051fc18
nabesh1n <shintaro.watanabe1226@gmail.com>
2018-09-23 14:09:36 +0900

    remove git pull origin master from prebench.sh

--- prebench.sh
+++ prebench.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /home/isucon/torb/webapp/go; git pull origin master; make
+cd /home/isucon/torb/webapp/go; make
 sudo systemctl restart h2o.service
 sudo systemctl restart mariadb.service
 sudo systemctl restart torb.go.service
