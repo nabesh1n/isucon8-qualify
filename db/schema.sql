1765743f5400a59b97bed39db43a3d195a82fa35
nabesh1n <shintaro.watanabe1226@gmail.com>
2018-09-24 20:47:01 +0900

    add event_id and canceled_at index to reservations

--- db/schema.sql
+++ db/schema.sql
@@ -30,7 +30,8 @@ CREATE TABLE IF NOT EXISTS reservations (
     reserved_at DATETIME(6)      NOT NULL,
     canceled_at DATETIME(6)      DEFAULT NULL,
     KEY event_id_and_sheet_id_idx (event_id, sheet_id),
-    INDEX (user_id, event_id)
+    INDEX (user_id, event_id),
+    INDEX (event_id, canceled_at)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS administrators (
